5a6fef68b82be05861e1f29702bb487725e5e3aa
eugene teu <eugeneteu@gmail.com>
2020-01-18 12:20:50 +0800

    created app

---
+++ android/app/src/main/kotlin/com/example/tree_secure/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tree_secure
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
