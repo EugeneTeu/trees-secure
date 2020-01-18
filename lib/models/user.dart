1721f85c7822afb17d3afc5e8f3b22d456567cc5
eugene teu <eugeneteu@gmail.com>
2020-01-18 16:14:32 +0800

    added user serializable

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,39 @@
+import 'package:json_annotation/json_annotation.dart';
 import 'package:tree_secure/models/base_class.dart';
 
-class User {}
+part 'user.g.dart';
+
+@JsonSerializable()
+class User extends BaseClass {
+  User(
+      {String id,
+      DateTime createdAt,
+      DateTime modifiedAt,
+      this.uuid,
+      this.location,
+      this.adoptedTrees})
+      : super(
+          id: id,
+          createdAt: createdAt,
+          modifiedAt: modifiedAt,
+        );
+
+  @JsonKey(name: 'name')
+  String name;
+  @JsonKey(name: 'uuid')
+  String uuid;
+  @JsonKey(name: 'location')
+  String location;
+  @JsonKey(name: 'adopted_trees')
+  List<String> adoptedTrees;
+
+  /// A necessary factory constructor for creating a new `User` instance
+  /// from a map. Pass the map to the generated `_$UserFromJson` constructor.
+  /// The constructor is named after the source class, in this case, `User`.
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  /// `toJson` is the convention for a class to declare support for serialization
+  /// to JSON. The implementation simply calls the private, generated
+  /// helper method `_$UserToJson`.
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
