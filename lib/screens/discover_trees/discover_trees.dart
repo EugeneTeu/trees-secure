f92f997fc072429a7dc17252d9fbb8e515594964
eugene teu <eugeneteu@gmail.com>
2020-01-18 18:36:57 +0800

    add web view

--- lib/screens/discover_trees/discover_trees.dart
+++ lib/screens/discover_trees/discover_trees.dart
@@ -27,10 +27,14 @@ class _DiscoverTreesState extends State<DiscoverTrees>
           children: <Widget>[
             Text("DISCOVER TREES"),
             Container(
-              height: 500,
+              height: MediaQuery.of(context).size.height / 3 * 2,
               width: 500,
               child: WebView(
-                initialUrl: 'https://en.wikipedia.org/wiki/Kraken',
+                javascriptMode: JavascriptMode.unrestricted,
+                initialUrl: Uri.dataFromString(
+                        '<html><body><iframe width="${MediaQuery.of(context).size.width * 3}" height="${MediaQuery.of(context).size.height * 2}" src="https://data.gov.sg/dataset/heritage-trees/resource/07df4d4c-274e-4b3a-af1d-7f79e43800d4/view/36d40023-2329-4a1f-9ea7-b49a680bc227" frameBorder="0"> </iframe></body></html>',
+                        mimeType: 'text/html')
+                    .toString(),
                 onWebViewCreated: (WebViewController webViewController) {
                   print("completed");
                   _controller.complete(webViewController);
