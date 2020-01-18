3fd40fea51bd04eb2ec7155525dd6343bffa76fb
aaroncql <choozy.aaron@gmail.com>
2020-01-18 16:56:30 +0800

    Add reading firestore user document

--- lib/models/user.g.dart
+++ lib/models/user.g.dart
@@ -11,10 +11,11 @@ User _$UserFromJson(Map<String, dynamic> json) {
     id: json['id'] as String,
     createdAt: BaseClass.convertTimestampToDateTime(json['created_at']),
     modifiedAt: BaseClass.convertTimestampToDateTime(json['modified_at']),
-    uuid: json['uuid'] as String,
     location: json['location'] as String,
     adoptedTrees:
         (json['adopted_trees'] as List)?.map((e) => e as String)?.toList(),
+    visitedTrees:
+        (json['visited_trees'] as List)?.map((e) => e as String)?.toList(),
   )..name = json['name'] as String;
 }
 
@@ -31,8 +32,8 @@ Map<String, dynamic> _$UserToJson(User instance) {
   writeNotNull('created_at', BaseClass.setDateTime(instance.createdAt));
   writeNotNull('modified_at', BaseClass.setDateTime(instance.modifiedAt));
   val['name'] = instance.name;
-  val['uuid'] = instance.uuid;
   val['location'] = instance.location;
   val['adopted_trees'] = instance.adoptedTrees;
+  val['visited_trees'] = instance.visitedTrees;
   return val;
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,51 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:tree_secure/services/auth_service.dart';
+import 'package:tree_secure/models/user.dart';
+import 'package:tree_secure/screens/splash/splash_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   final AuthService _auth = AuthService.instance;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-          child: ListView(
-        padding: const EdgeInsets.all(0.0),
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-            margin: const EdgeInsets.all(0.0),
-            currentAccountPicture: CircleAvatar(
-              radius: 30.0,
-              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
-              backgroundColor: Colors.transparent,
+    final User user = Provider.of<User>(context);
+
+    return user == null
+        ? SplashScreen()
+        : Scaffold(
+            drawer: Drawer(
+              child: ListView(
+                padding: const EdgeInsets.all(0.0),
+                children: <Widget>[
+                  UserAccountsDrawerHeader(
+                    margin: const EdgeInsets.all(0.0),
+                    currentAccountPicture: CircleAvatar(
+                      radius: 30.0,
+                      backgroundImage:
+                          NetworkImage('https://via.placeholder.com/150'),
+                      backgroundColor: Colors.transparent,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    accountName: Text("user account name"),
+                    accountEmail: Text("user account email"),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    title: Text('Adopted Trees'),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    title: Text('Visited Trees'),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      this._auth.signOut();
+                    },
+                    title: Text('Logout'),
+                  )
+                ],
+              ),
             ),
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
+            appBar: AppBar(
+              title: Text('Tree Secure'),
+              actions: <Widget>[],
             ),
-            accountName: Text("user account name"),
-            accountEmail: Text("user account email"),
-          ),
-          ListTile(
-            onTap: () {},
-            title: Text('Adopted Trees'),
-          ),
-          ListTile(
-            onTap: () {},
-            title: Text('Visited Trees'),
-          ),
-          ListTile(
-            onTap: () {
-              this._auth.signOut();
-            },
-            title: Text('Logout'),
-          )
-        ],
-      )),
-      appBar: AppBar(
-        title: Text('Tree Secure'),
-        actions: <Widget>[],
-      ),
-      body: Text('LOGGED IN'),
-    );
+            body: Text('LOGGED IN'),
+          );
   }
 }
