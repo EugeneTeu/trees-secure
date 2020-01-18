86f45352663e0c229aa707c985865fde3ba2f640
eugene teu <eugeneteu@gmail.com>
2020-01-18 18:18:15 +0800

    added extra web view

--- lib/screens/adopted_trees/adopted_trees.dart
+++ lib/screens/adopted_trees/adopted_trees.dart
@@ -7,6 +7,7 @@ class AdoptedTrees extends StatefulWidget {
 
 class _AdoptedTreesState extends State<AdoptedTrees>
     with AutomaticKeepAliveClientMixin {
+
   final List<Widget> listOfAdoptedTrees = [Text("dadas")];
 
   @override
@@ -19,31 +20,28 @@ class _AdoptedTreesState extends State<AdoptedTrees>
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SizedBox(height: 8.0),
-          ListTile(
-            enabled: false,
-            title: Text("Number of Trees"),
-            subtitle: Text("num of trees here"),
-          ),
-          Text("These are your adopted Trees"),
-          SizedBox(height: 8.0),
-          Container(
-            width: 200,
-            height: MediaQuery.of(context).size.height / 2,
-            child: SingleChildScrollView(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return listOfAdoptedTrees[index];
-                },
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            SizedBox(height: 8.0),
+            ListTile(
+              enabled: false,
+              title: Text("Number of Trees"),
+              subtitle: Text("num of trees here"),
             ),
-          )
-        ],
+            Text("These are your adopted Trees"),
+            SizedBox(height: 8.0),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: 1,
+              itemBuilder: (BuildContext context, int index) {
+                return listOfAdoptedTrees[index];
+              },
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screens/discover_trees/discover_trees.dart
+++ lib/screens/discover_trees/discover_trees.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class DiscoverTrees extends StatefulWidget {
   @override
@@ -9,18 +12,33 @@ class DiscoverTrees extends StatefulWidget {
 
 class _DiscoverTreesState extends State<DiscoverTrees>
     with AutomaticKeepAliveClientMixin {
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+  final Set<String> _favorites = Set<String>();
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text("DISCOVER TREES"),
-          Text("This Feature is underway")
-        ],
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("DISCOVER TREES"),
+            Container(
+              height: 500,
+              width: 500,
+              child: WebView(
+                initialUrl: 'https://en.wikipedia.org/wiki/Kraken',
+                onWebViewCreated: (WebViewController webViewController) {
+                  print("completed");
+                  _controller.complete(webViewController);
+                },
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
