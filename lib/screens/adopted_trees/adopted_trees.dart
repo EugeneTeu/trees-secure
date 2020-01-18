c6ae497e5be183d32f714c84729ca10ac7fa4c04
eugene teu <eugeneteu@gmail.com>
2020-01-18 20:32:45 +0800

    add list tile

--- lib/screens/adopted_trees/adopted_trees.dart
+++ lib/screens/adopted_trees/adopted_trees.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:tree_secure/models/tree.dart';
+import 'package:tree_secure/models/user.dart';
+import 'package:tree_secure/shared/loading_spinner.dart';
 
 class AdoptedTrees extends StatefulWidget {
   @override
@@ -7,43 +14,98 @@ class AdoptedTrees extends StatefulWidget {
 
 class _AdoptedTreesState extends State<AdoptedTrees>
     with AutomaticKeepAliveClientMixin {
-
-  final List<Widget> listOfAdoptedTrees = [Text("dadas")];
+  final List<Widget> listOfAdoptedTrees = [];
+  final List<Tree> listOfTree = [];
+  bool isLoading;
 
   @override
   void initState() {
     super.initState();
+    isLoading = true;
     //load from provider here
+    loadTreeFromJson().then((value) {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
+  loadTreeFromJson() async {
+    String data = await rootBundle.loadString("assets/formatted_trees.json");
+    Map<String, dynamic> jsonResult = json.decode(data);
+    jsonResult.forEach((String key, dynamic object) {
+      Tree temp = Tree();
+      temp.commonName = object['Common name'];
+      temp.id = key;
+      listOfTree.add(temp);
+    });
+
+    listOfTree.forEach((tree) {
+      var temp = ListTile(
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          title: Text("Tree ID: ${tree.id}"),
+          leading: Container(
+            padding: EdgeInsets.only(right: 12.0),
+            decoration: BoxDecoration(
+                border:
+                    Border(right: BorderSide(width: 1.0, color: Colors.black))),
+            child: Icon(Icons.dashboard, color: Colors.black),
+          ),
+          subtitle: Row(
+            children: <Widget>[
+              Icon(Icons.terrain, color: Theme.of(context).primaryColorDark),
+              Text(" TREEE", style: TextStyle(color: Colors.black))
+            ],
+          ),
+          trailing: Icon(Icons.keyboard_arrow_right,
+              color: Colors.black, size: 30.0));
+      listOfAdoptedTrees.add(temp);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final User user = Provider.of<User>(context);
+
     super.build(context);
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            SizedBox(height: 8.0),
-            ListTile(
-              enabled: false,
-              title: Text("Number of Trees"),
-              subtitle: Text("num of trees here"),
+    return isLoading
+        ? LoadingSpinner()
+        : Scaffold(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                SizedBox(height: 8.0),
+                Row(),
+                ListTile(
+                  enabled: true,
+                  title: Text("Number of Trees Visited:"),
+                  trailing: Text('${user.visitedTrees.length}'),
+                ),
+                ListTile(
+                  enabled: true,
+                  title: Text("Number of Trees Adopted:"),
+                  trailing: Text('${user.adoptedTrees.length}'),
+                ),
+                Text("These are your adopted Trees"),
+                SizedBox(height: 8.0),
+                Container(
+                  height: MediaQuery.of(context).size.height / 3 * 2,
+                  child: ListView.builder(
+                    physics: AlwaysScrollableScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    cacheExtent: 20.0,
+                    shrinkWrap: true,
+                    itemCount: 255,
+                    itemBuilder: (BuildContext context, int index) {
+                      return listOfAdoptedTrees[index];
+                    },
+                  ),
+                )
+              ],
             ),
-            Text("These are your adopted Trees"),
-            SizedBox(height: 8.0),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: 1,
-              itemBuilder: (BuildContext context, int index) {
-                return listOfAdoptedTrees[index];
-              },
-            )
-          ],
-        ),
-      ),
-    );
+          );
   }
 
   @override
